afc9fa5c7e79a9810df247493d150112b5007321
Paul Brack <paul.brack@manchester.ac.uk>
2019-03-29 19:27:00 +0000

    Update readme. Add start of MZML parsing code.

--- MzmlReader/MzmlParser.cs
+++ MzmlReader/MzmlParser.cs
@@ -15,44 +15,24 @@ namespace MzmlParser
         private static Logger logger = LogManager.GetCurrentClassLogger();
         private string lastScanRead = String.Empty;
 
-        //public Library LoadLibrary(string path)
-        //{
-        //    logger.Info("Loading file: {0}", path);
-
-        //    Library library = new Library();
-        //    using (XmlReader reader = XmlReader.Create(path))
-        //    {
-        //        while (reader.Read())
-        //        {
-        //            if (reader.IsStartElement())
-        //            {
-        //                switch (reader.LocalName)
-        //                {
-        //                    case "Protein":
-        //                        AddProtein(library, reader);
-        //                        break;
-        //                    case "ProteinRef":
-        //                        AddPeptideReference(library, reader);
-        //                        break;
-        //                    case "Peptide":
-        //                        lastPeptideRead = reader.GetAttribute("id");
-        //                        AddPeptide(library, reader);
-        //                        break;
-        //                    case "Transition":
-        //                        AddTransition(library, reader);
-        //                        break;
-        //                }
-        //            }
-        //        }
-        //        logger.Debug("{0} proteins loaded", library.ProteinList.Count);
-        //        logger.Debug("{0} unique uniprot IDs", library.UniprotIdList.Count);
-        //        logger.Debug("{0} decoy proteins loaded", library.ProteinDecoyList.Count);
-        //        logger.Debug("{0} peptides loaded", library.PeptideList.Count);
-        //        logger.Debug("{0} IRT peptides loaded", library.RtList.Count);
-        //        logger.Debug("{0} transitions loaded", library.TransitionList.Count);
-        //    }
-        //    System.IO.File.WriteAllLines("proteins.txt", library.UniprotIdList.Values.Cast<string>());
-        //    return library;
-        //}
+        public void LoadMzml(string path)
+        {
+            logger.Info("Loading file: {0}", path);
+            using (XmlReader reader = XmlReader.Create(path))
+            {
+                while (reader.Read())
+                {
+                    if (reader.IsStartElement())
+                    {
+                        switch (reader.LocalName)
+                        {
+                            case "Protein":
+                                //do summat
+                                break;
+                        }
+                    }
+                }
+            }
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Yamato
-SWATH-QC metrics
+SWATH-QC metrics generation tools
+
+# MzmlReader
+A library to extract chromatograms and metadata from an MZML file
+
+# SwaMe
+A Quameter-based SWATH metrics library
+Author: Marina Pauw
+pauwmarina@sun.ac.za
+
+# Prognosticator
+A library to try and figure out if your DIA proteomics experiment is going okay
+Author: Paul Brack
+paul.brack@manchester.ac.uk
+
+# Yamato.Console
+A command line application to generate SwaMe and Prognosticator metrics
+
