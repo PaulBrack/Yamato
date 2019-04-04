0bb993c1f75b28205863c7fddcab922b98fbf1cb
Paul Brack <paul.brack@manchester.ac.uk>
2019-04-04 03:47:40 +0100

    Remove unwated usings and white space

--- Console/Program.cs
+++ Console/Program.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Diagnostics;
 using NLog;
-using System.Threading;
 
 namespace Yamato.Console
 {
--- SwaMe/MetricGenerator.cs
+++ SwaMe/MetricGenerator.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using MzmlParser;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace SwaMe
+﻿namespace SwaMe
 {
     public class MetricGenerator
     {
