83e895f8040fc95c1fc0ffd11341631554092360
Paul Brack <paul.brack@manchester.ac.uk>
2019-04-04 10:50:23 +0100

    Add SwaMe stub back in

--- SwaMe/MetricGenerator.cs
+++ SwaMe/MetricGenerator.cs
@@ -1,7 +1,13 @@
-﻿namespace SwaMe
+﻿using MzmlParser;
+
+namespace SwaMe
 {
     public class MetricGenerator
     {
-
+        public void GenerateMetrics(Run run)
+        {
+            //Put a breakpoint somewhere in this method to explore the run object
+            
+        }
     }
 }
