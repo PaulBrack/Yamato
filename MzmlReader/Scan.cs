6101f50200d062488752e121757d9781566f7e64
Paul Brack <paul.brack@manchester.ac.uk>
2019-04-15 13:40:19 +0100

    Add support for 64 bit numbers and check for Zlib compression (no support for this yet)

--- MzmlReader/Scan.cs
+++ MzmlReader/Scan.cs
@@ -6,7 +6,7 @@
         public int? MsLevel { get; set; }
         public double BasePeakIntensity { get; set; }
         public double BasePeakMz { get; set; }
-        public double TotalIonCurrent { get; set; }  
+        public double TotalIonCurrent { get; set; }
         public double ScanStartTime { get; set; }
         public double IsolationWindowTargetMz { get; set; }
         public double IsolationWindowUpperOffset { get; set; }
@@ -15,12 +15,17 @@
 
     public class ScanAndTempProperties
     {
-        public ScanAndTempProperties() {
+        public ScanAndTempProperties()
+        {
             Scan = new Scan();
-            }
+        }
 
         public Scan Scan { get; set; }
         public string Base64IntensityArray { get; set; }
         public string Base64MzArray { get; set; }
+        public bool IntensityZlibCompressed { get; set; }
+        public bool MzZlibCompressed { get; set; }
+        public int IntensityBitLength { get; set; }
+        public int MzBitLength { get; set; }
     }
 }
