ba388e4c1f3bd18548b1f36cdf4e8f4ef1993a71
mbrxspb5 <paul.brack@manchester.ac.uk>
2019-05-09 12:07:28 +0100

    Rename parser project folder

--- LibraryParser/SkyReader.cs
+++ LibraryParser/SkyReader.cs
@@ -6,6 +6,7 @@ using System.Xml;
 using System.Xml.Linq;
 using System.Linq;
 
+
 namespace LibraryParser
 {
     public class SkyReader
@@ -26,14 +27,14 @@ namespace LibraryParser
                     {
                         switch (reader.LocalName)
                         {
-                            case "protein":
+                            case "Protein":
                                 AddProtein(library, reader);
                                 break;
-                            case "peptide":
+                            case "Peptide":
                                 lastPeptideRead = reader.GetAttribute("id");
                                 AddPeptide(library, reader);
                                 break;
-                            case "transition":
+                            case "Transition":
                                 AddTransition(library, reader);
                                 break;
                         }
@@ -69,87 +70,32 @@ namespace LibraryParser
         private void AddPeptide(Library library, XmlReader reader)
         {
             var peptide = new Library.Peptide();
-            bool peptideRead = false;
-
-            while (reader.Read() && !peptideRead)
-            {
-                if (reader.IsStartElement())
-                {
-
-                    peptide.AssociatedTransitionIds = new List<string>();
-                    if (reader.LocalName == "precursor")
-                    {
-                        peptide.ChargeState = Convert.ToInt32(reader.GetAttribute("charge"));
-                        peptide.Id = reader.GetAttribute("precursor_mz");
-                    }
-                    else if (reader.LocalName == "peptide")
-                    {
-                        peptide.Sequence = reader.GetAttribute("sequence");
-
-                        peptide.RetentionTime = Convert.ToInt32(reader.GetAttribute("ave_retention_time"));
-                    }
-                    else if (reader.LocalName == "collision_energy")
-                    {
-
-                        peptide.CollisionEnergy = Convert.ToDouble(reader.GetAttribute("collision_energy"));
-                    }
-
-                    }
-                else if ( reader.LocalName == "transition")
-                {
-                    peptideRead = true;
-                }
-            }
+            peptide.Id = reader.GetAttribute("name");
+            peptide.Sequence = reader.GetAttribute("sequence");
+            peptide.AssociatedTransitionIds = new List<string>();
+            peptide.ChargeState = Convert.ToInt32(reader.GetAttribute("charge"));
+            peptide.RetentionTime = Convert.ToInt32(reader.GetAttribute("ave_retention"));
+                                
 
             library.PeptideList.Add(peptide.Id, peptide);
         }
 
-        private void AddPeptideReference(Library library, XmlReader xmlReader)
-        {
-            string proteinRef = xmlReader.GetAttribute("ref");
-            if (!proteinRef.StartsWith("DECOY"))
-            {
-                Library.Protein correspondingProtein = (Library.Protein)(library.ProteinList[proteinRef]);
-                correspondingProtein.AssociatedPeptideIds.Add(lastPeptideRead);
-            }
-            else
-            {
-                Library.Protein correspondingProtein = (Library.Protein)(library.ProteinDecoyList[proteinRef]);
-                correspondingProtein.AssociatedPeptideIds.Add(lastPeptideRead);
-            }
-        }
-
+       
         private void AddTransition(Library library, XmlReader reader)
         {
             var transition = new Library.Transition();
-            bool transRead = false;
-            while (reader.Read() && !transRead)
-            {
-                if (reader.IsStartElement())
-                {
-                    if (reader.LocalName == "precursor_mz")
-                    {
-                        transition.PeptideId = reader.GetAttribute("precursor_mz");
-                        transition.PrecursorMz = Convert.ToDouble(reader.GetAttribute("precursor_mz"));
-                    }
-                    else if (reader.LocalName == "transition")
-                    {
-                        transition.Id = String.Concat(reader.GetAttribute("fragment_type") , reader.GetAttribute("fragment_ordinal"));
-                        transition.IonType = reader.GetAttribute("fragment_type");
-                        transition.ProductIonSeriesOrdinal = Convert.ToInt32(reader.GetAttribute("fragment_ordinal"));
-                        transition.ProductIonChargeState = Convert.ToInt32(reader.GetAttribute("product_charge"));
-                    }
-                    
-                    else if (reader.LocalName == "product_mz")
-                    {
-                        transition.ProductMz = Convert.ToDouble(reader.GetAttribute("product_mz"));
-                    }
-                }
-                else if (reader.NodeType == XmlNodeType.EndElement && reader.LocalName == "transition")
-                {
-                    transRead = true;
-                }
-            }
+            transition.PeptideId = reader.GetAttribute("precursor_mz");
+            transition.Id = reader.GetAttribute("product_mz");
+            Enums.IonType? ionType = null;
+            transition.ProductMz = Convert.ToDouble(reader.GetAttribute("product_mz"));
+            transition.PrecursorMz = Convert.ToDouble(reader.GetAttribute("precursor_mz"));
+            transition.ProductIonChargeState = Convert.ToInt32(reader.GetAttribute("product_charge"));
+            transition.ProductIonSeriesOrdinal = Convert.ToInt32(reader.GetAttribute("fragment_ordinal"));
+            transition.IonType = reader.GetAttribute("fragment_type");
+            transition.ProductIonIntensity = Convert.ToDouble(reader.GetAttribute("height"));
+
+
+            
 
             library.TransitionList.Add(transition.Id, transition);
             var correspondingPeptide = (Library.Peptide)(library.PeptideList[transition.PeptideId]);
@@ -169,9 +115,5 @@ namespace LibraryParser
                 }
             }
         }
-
     }
-
-    
-
 }
