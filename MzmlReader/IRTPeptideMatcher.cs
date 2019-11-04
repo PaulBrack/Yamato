eed32af7550e4534102e69607e6d30a014e9ecba
Marina Pauw <pauwmarina@gmail.com>
2019-11-04 09:54:00 +0200

    Changes made to produce chromatogrammetrics from iRTpeaks

--- MzmlReader/IRTPeptideMatcher.cs
+++ MzmlReader/IRTPeptideMatcher.cs
@@ -3,6 +3,7 @@ using System.Collections.Concurrent;
 using System.Threading;
 using System;
 using LibraryParser;
+using System.Collections.Generic;
 
 namespace MzmlParser
 {
@@ -37,27 +38,35 @@ namespace MzmlParser
 
         private static void FindIrtSpectra(Run run, CandidateHit candidateHit)
         {
-            var matchingScans = run.Ms2Scans.Where(x => Math.Abs(x.ScanStartTime - candidateHit.RetentionTime) < run.AnalysisSettings.RtTolerance
+            var matchingScans = run.Ms2Scans.Where(x => Math.Abs(x.ScanStartTime - candidateHit.RetentionTime) < run.AnalysisSettings.RtTolerance/*
                                && candidateHit.PrecursorTargetMz > x.IsolationWindowLowerBoundary
-                               && candidateHit.PrecursorTargetMz < x.IsolationWindowUpperBoundary);
-
+                               && candidateHit.PrecursorTargetMz < x.IsolationWindowUpperBoundary*/);
+           
             var libraryPeptides = run.AnalysisSettings.IrtLibrary.PeptideList.Values.Cast<Library.Peptide>().ToList();
+            List<SpectrumPoint> matchingSpectrum = new List<SpectrumPoint>();
+            Library.Peptide peptide = new Library.Peptide();
 
             foreach (var matchingScan in matchingScans)
             {
                 foreach (double targetMz in candidateHit.ProductTargetMzs)
                 {
-                    var matchingSpectrum = matchingScan.Spectrum.Where(x => Math.Abs(x.Mz - targetMz) < run.AnalysisSettings.IrtMassTolerance).ToList();
-                    if (matchingSpectrum.Any())
-                    {
-                        var peptide = libraryPeptides.Where(x => x.Sequence == candidateHit.PeptideSequence).Single();
-                        IRTPeak iRTPeak = new IRTPeak();
-                        iRTPeak.AssociatedTransitions = peptide.AssociatedTransitions;
-                        iRTPeak.BasePeak = new SpectrumPoint() { Intensity = (float)matchingScan.BasePeakIntensity, Mz = (float)matchingScan.BasePeakMz, RetentionTime = (float)matchingScan.ScanStartTime };
-                        run.IRTPeaks.Add(iRTPeak);
-                    }
+                    var match = matchingScan.Spectrum.Where(x => Math.Abs(x.Mz - targetMz) < run.AnalysisSettings.IrtMassTolerance).ToList();
+                    if(match.Any())
+                    matchingSpectrum.Add(match.First());//In a scan only one point is taken for each transition
                 }
             }
+            if (matchingSpectrum.Any())
+            {
+                peptide = libraryPeptides.Where(x => x.Sequence == candidateHit.PeptideSequence).Single();
+
+            }
+
+            IRTPeak iRTPeak = new IRTPeak();
+            iRTPeak.RetentionTime = candidateHit.RetentionTime;
+            iRTPeak.Mz = candidateHit.PrecursorTargetMz;
+            iRTPeak.AssociatedTransitions = peptide.AssociatedTransitions;
+            iRTPeak.Spectrum = matchingSpectrum;
+            run.IRTPeaks.Add(iRTPeak);
             cde.Signal();
         }
 
