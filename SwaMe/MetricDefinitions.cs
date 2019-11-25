c6a695296f6d241b5b860c9d1b8e1224376a1485
mbrxspb5 <paul.brack@manchester.ac.uk>
2019-11-25 16:46:03 +0000

    Remove mistakenly committed test files from SwaMe

--- SwaMe/MetricDefinitions.cs
+++ SwaMe/MetricDefinitions.cs
@@ -4,6 +4,8 @@ using System.Text;
 
 namespace SwaMe
 {
+
+    //this is a stub so later on we can define metrics in a class file here and use them to build a CSV more safely and dynamically than at present
     class MetricDefinitions
     {
     }
