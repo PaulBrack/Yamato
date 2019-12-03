30a84292ce17f85f738945c1d226c817b646277c
mbrxspb5 <paul.brack@manchester.ac.uk>
2019-12-03 18:20:11 +0000

    Create test context Run

--- MzmlParser.Test/MzmlReader.cs
+++ MzmlParser.Test/MzmlReader.cs
@@ -1,10 +1,31 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
 
 namespace MzmlParser.Test
 {
+
+
+
     [TestClass]
     public class MzmlReaderTest
     {
+        private static Run run;
+
+        private static AnalysisSettings analysisSettings = new AnalysisSettings()
+        {
+            MassTolerance = 0.05,
+            RtTolerance = 5,
+            IrtMinIntensity = 250,
+            IrtMinPeptides = 3,
+            IrtMassTolerance = 0.05
+        };
+
+        [ClassInitialize]
+        public static void Initialize(TestContext t)
+        {
+            run = new MzmlReader().LoadMzml(Path.Combine("mzmls", "test.mzml"), true, analysisSettings);
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
