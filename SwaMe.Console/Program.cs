e0c9a48b5eda9981c68ea1d647ed46c4bbcfa310
Marina Pauw <pauwmarina@gmail.com>
2020-03-03 19:22:52 +0200

    If statement to enable using a csv/tsv file as input for iRT

--- SwaMe.Console/Program.cs
+++ SwaMe.Console/Program.cs
@@ -88,8 +88,17 @@ namespace Yamato.Console
                     if (!String.IsNullOrEmpty(options.IRTFile))
                     {
                         irt = true;
-                        TraMLReader traMLReader = new TraMLReader();
-                        analysisSettings.IrtLibrary = traMLReader.LoadLibrary(options.IRTFile);
+                        if (options.IRTFile.ToLower().EndsWith("traml"))
+                        {
+                            TraMLReader traMLReader = new TraMLReader();
+                            analysisSettings.IrtLibrary = traMLReader.LoadLibrary(options.IRTFile);
+
+                        }
+                        else if (options.IRTFile.ToLower().EndsWith("tsv") || options.IRTFile.ToLower().EndsWith("csv")) 
+                        {
+                            SVReader svReader = new SVReader();
+                            analysisSettings.IrtLibrary = svReader.LoadLibrary(options.IRTFile);
+                        }
                     }
                     MzmlParser.Run run = mzmlParser.LoadMzml(inputFilePath, analysisSettings);
                     AnalysisSettingsFileWriter Aw = new AnalysisSettingsFileWriter();
