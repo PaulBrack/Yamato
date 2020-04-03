918106457d165e2063e088407b581fe4e82c2194
Peter Crowther <peter.crowther@melandra.com>
2020-04-03 13:24:59 +0100

    Fix #151: use exceptions for file format errors in CSVs

---
+++ LibraryParser/SVFileFormatException.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace LibraryParser
+{
+    /// <summary>
+    /// Designed to be thrown when the SV parser detects an unfixable format error in the file it's parsing.
+    /// </summary>
+    [Serializable]
+    internal class SVFileFormatException : Exception
+    {
+        public string Path { get; set; }
+        public SVFileFormatException()
+        {
+        }
+
+        public SVFileFormatException(string message) : base(message)
+        {
+        }
+
+        public SVFileFormatException(string message, string path) : base(message)
+        {
+            Path = path;
+        }
+
+        public SVFileFormatException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        protected SVFileFormatException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
