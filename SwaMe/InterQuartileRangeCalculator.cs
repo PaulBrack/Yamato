83874425a1da0e3bcc850fd38a70429089980b0c
Peter Crowther <peter.crowther@melandra.com>
2021-02-25 09:59:50 +0000

    Improve docs on InterQuartileRangeCalculator

--- SwaMe/InterQuartileRangeCalculator.cs
+++ SwaMe/InterQuartileRangeCalculator.cs
@@ -5,28 +5,27 @@ namespace SwaMe
 {
     public static class InterQuartileRangeCalculator
     {
+        /// <summary>
+        /// Precondition: list must be sorted ascending.
+        /// </summary>
         public static double CalcIQR(List<double> list)
         {
-            if (list.Count < 5) 
-            {
-            throw new ArgumentException(String.Format("{0} is too few list items to calculate IQR", list.Count),
-                                      nameof(list));
-            }
-            // Note list must already be sorted.
+            if (list.Count < 5)
+                throw new ArgumentException($"At least 5 list items required to calculate IQR, got {list.Count}", nameof(list));
+
             int length = list.Count;
             double Q1 = list[length / 4];
             double Q3 = list[length / 4 * 3];
             return Q3 - Q1;
         }
 
+        /// <summary>
+        /// Precondition: list must be sorted ascending.
+        /// </summary>
         public static int CalcIQR(List<int> list)
         {
-            // Note list must already be sorted.
             if (list.Count < 5)
-            {
-                throw new ArgumentException(String.Format("{0} is too few list items to calculate IQR", list.Count),
-                                          nameof(list));
-            }
+                throw new ArgumentException($"At least 5 list items required to calculate IQR, got {list.Count}", nameof(list));
             int length = list.Count;
             int Q1 = list[length / 4];
             int Q3 = list[length / 4 * 3];
