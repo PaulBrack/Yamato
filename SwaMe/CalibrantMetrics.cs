781d3fffe4964789edd41da33c8b37ac68d347dd
Peter Crowther <peter.crowther@melandra.com>
2021-03-02 17:31:14 +0000

    Match naming and fix properties of CalibrantMetrics

---
+++ SwaMe/CalibrantMetrics.cs
@@ -0,0 +1,20 @@
+﻿#nullable enable
+
+using System.Collections.Generic;
+
+namespace SwaMe
+{
+    public class CalibrantMetrics
+    {
+        public List<double> PeakWidths { get; }
+        public List<double> TailingFactor { get; }
+        public List<double> PeakPrecision { get; }
+
+        public CalibrantMetrics(List<double> peakWidths, List<double> tailingFactor, List<double> peakPrecision)
+        {
+            PeakWidths = peakWidths;
+            TailingFactor = tailingFactor;
+            PeakPrecision = peakPrecision;
+        }
+    }
+}
